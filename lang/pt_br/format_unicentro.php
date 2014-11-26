2e04c04d686d654ffeca27cfbed9298af72f49c0
Tony Alexander Hild <tony_hild@yahoo.com>
2014-11-26 13:58:58 -0200

    First commit

---
+++ lang/pt_br/format_unicentro.php
@@ -0,0 +1,33 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'format_unicentro', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   format_unicentro
+ * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['currentsection'] = 'Este tópico';
+$string['sectionname'] = 'Tópico';
+$string['pluginname'] = 'Formato Unicentro';
+$string['section0name'] = 'Geral';
+$string['page-course-view-unicentro'] = 'Qualquer página principal no formato Unicentro';
+$string['page-course-view-unicentro-x'] = 'Qualquer página de curso no formato Unicentro';
+$string['hidefromothers'] = 'Esconder tópico';
+$string['showfromothers'] = 'Mostrar tópico';
